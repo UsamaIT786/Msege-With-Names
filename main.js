dcac0d49f22bd2f600858944421a044e961d11b9
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-15 18:16:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Assignment Question
+//Task 12
+var names = ["Usama", "Bilal", "Hammad", "Arsalan", "Farhan"];
+var mesege = "Hello Dear are you Web Developer?";
+console.log(names[0] + " " + mesege);
+console.log(names[1] + " " + mesege);
+console.log(names[2] + " " + mesege);
+console.log(names[3] + " " + mesege);
+console.log(names[4] + " " + mesege);
---
+++ main.ts
@@ -0,0 +1,10 @@
+//Assignment Question
+//Task 12
+
+let names : string[] = ["Usama","Bilal","Hammad","Arsalan","Farhan"];
+let mesege : String = "Hello Dear are you Web Developer?"
+console.log(names[0] + " " + mesege);
+console.log(names[1] + " " + mesege);
+console.log(names[2] + " " + mesege);
+console.log(names[3] + " " + mesege);
+console.log(names[4] + " " + mesege);
